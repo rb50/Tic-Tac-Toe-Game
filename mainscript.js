7622045466e6d39a4054bcfeaebce2f63dd3f4bf
Rebecca Botha <rebeccabotha@Rebeccas-MacBook-Air.local>
2017-09-20 18:13:44 +0100

    add initial computer and player turns

--- mainscript.js
+++ mainscript.js
@@ -26,6 +26,7 @@ window.onload = function() {
     computerStatus.turn = 'yes';
     playerStatus.turn = 'no';
     gridDisplay(grid);
+    setTimeout(function () {gamePlay()}, 3000);
 
   }
 
@@ -35,18 +36,63 @@ window.onload = function() {
     computerStatus.turn = 'yes';
     playerStatus.turn = 'no';
     gridDisplay(grid);
+    setTimeout(function () {gamePlay()}, 3000);
   }
 
   function gridDisplay(grid){
     document.getElementById("grid").innerHTML = grid;
-    document.getElementById("playersturn").style.display ="inline-block";
+    document.getElementById("computersturn").style.display ="inline-block";
     document.getElementById("grid").style.top ="-27px";
   }
 
-  for(key in gridStatus) {
-    document.getElementById(prop).addEventListener('click')
+  function gamePlay() {
+    if(computerStatus.letter === 'X') {
+      document.getElementById('b2').innerHTML = "<p class='turns'>X</p>";
+    } else if (computerStatus.letter === 'O') {
+      document.getElementById('b2').innerHTML = "<p class='turns'>O</p>";
+    }
 
+    document.getElementById('computersturn').innerHTML = "Player's Turn"
+
+
+
+    var squares = ["a1", "a2", "a3", "b1", "b3", "c1", "c2", "c3"]
+
+    function playersTurn(){
+
+      function listeners() {
+        for(let i = 0; i < squares.length; i++){
+          document.getElementById(squares[i]).addEventListener('click', clicker, false)
+        }
+      }
+
+      function removeListeners(e){
+        for(let i = 0; i < squares.length; i++){
+          document.getElementById(squares[i]).removeEventListener('click', clicker, false)
+        }
+        delete squares[squares.indexOf(e.target.id)];
+
+      }
+
+      function clicker(e) {
+        e.target.innerHTML = "<p class='turns'>"+playerStatus.letter+"</p>";
+
+        removeListeners(e);
+        
+
+      }
+      listeners();
+
+
+
+    }
+
+    playersTurn();
   }
+
+
+
+
 
 
 
